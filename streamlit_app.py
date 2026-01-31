45ee2e9ad254028352d8f49656f89fc8e21e3511
privateezhik-netizen <privateezhik@gmail.com>
2026-01-30 20:52:06 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,8 +28,8 @@ session = cnx.session()
 #get_active_session() # removed from above
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
-
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 
 
